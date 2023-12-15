e869d3f50c874aedd85f5d9fd4d5c30e48c1245f
*Lui <luimatthew.orpiano.cics@ust.edu.ph>
2023-12-15 17:44:26 +0800

    connecting fragments

--- app/src/main/java/com/example/finalsproject/ProfileInterface.kt
+++ app/src/main/java/com/example/finalsproject/ProfileInterface.kt
@@ -1,11 +1,25 @@
 package com.example.finalsproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ProfileInterface : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_interface)
+
+        val buttonply = findViewById<ImageButton>(R.id.imgBtnplay)
+        buttonply.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonbck = findViewById<ImageButton>(R.id.imgbtnBACK)
+        buttonbck.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
